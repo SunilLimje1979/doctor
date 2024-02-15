a7fbd5239ef39d6135d1b2d2687466aa33f9b776
unknown <SunilLimje1979>
2024-02-15 11:48:21 +0530

    setting commit

--- doctor/urls.py
+++ doctor/urls.py
@@ -19,6 +19,6 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("api/",include("doctorapp.urls")),
+    path('doctor/admin/', admin.site.urls),
+    path("doctor/api/",include("doctorapp.urls")),
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
