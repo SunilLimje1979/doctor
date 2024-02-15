35ee2a1ecbf5a02ab7f91cd699a248f16ce3a446
unknown <SunilLimje1979>
2024-02-15 10:27:09 +0530

    setting commit

--- doctor/settings.py
+++ doctor/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-2s-(y@%e9&vjo6fdvpy3mi2(x44hb&e-_0!@ynz6z^60z9e2d4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*","localhost","localhost:5432","0.0.0.0:8000","13.233.211.102","13.233.211.102:8000"]
 
 
 # Application definition
@@ -75,19 +75,14 @@ WSGI_APPLICATION = 'doctor.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'db_gyaagl_vocabuli_medicify',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '3306',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        },
+        'USER': 'postgres',
+        'PASSWORD': 'Medicify@2024',
+        'HOST': '13.233.211.102',
+        'PORT': '5432',
     }
 }
 
